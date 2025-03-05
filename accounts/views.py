f875a22e501b687a26ba8d2625e981e7b7d7fcae
ouxrlo <ouxrlo3@gmail.com>
2025-03-05 16:16:11 +0900

    fix: code change n add migrations

--- accounts/views.py
+++ accounts/views.py
@@ -103,7 +103,7 @@ class FindUsernameView(generics.GenericAPIView):
                 user = User.objects.get(email=email)
                 return Response({"username": user.username}, status=status.HTTP_200_OK)
             except User.DoesNotExist:
-                return Response({"email": ["해당 이메일로 등록된 계정이 없습니다."]}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"email": ("해당 이메일로 등록된 계정이 없습니다.")}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
