6823e162537a577ea1fcda9377bb27b2826368fb
ouxrlo <ouxrlo3@gmail.com>
2025-03-06 18:09:24 +0900

    fix: code change

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterView, LoginView, TokenRefreshView, PregnancyViewSet, FindUsernameView
+from .views import (
+    RegisterView, LoginView, TokenRefreshView, PregnancyViewSet
+)
 
 pregnancy_router = DefaultRouter()
 pregnancy_router.register(r'pregnancies', PregnancyViewSet, basename='pregnancy')
@@ -8,7 +10,6 @@ pregnancy_router.register(r'pregnancies', PregnancyViewSet, basename='pregnancy'
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),  # 회원가입
     path('login/', LoginView.as_view(), name='login'),  # 로그인
-    path("find_id/", FindUsernameView.as_view(), name="find-id"),   # 아이디 찾기
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # 토큰 갱신
     path('', include(pregnancy_router.urls)),  # 임신 관련 API
 ]
