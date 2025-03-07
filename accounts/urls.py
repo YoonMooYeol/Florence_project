5801dfc87b7fe2eabdb5673f8c91252bbbe49486
YoonMooYeol <agecross@naver.com>
2025-03-07 19:24:03 +0900

    feat: 카카오 소셜 로그인 구현
    - 카카오 OAuth 인증 콜백 뷰 추가
    - 카카오 액세스 토큰 및 사용자 프로필 정보 처리
    - 신규/기존 사용자 자동 로그인 및 JWT 토큰 생성
    - CORS 설정에 프론트엔드 개발 서버 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterView, LoginView, TokenRefreshView, PregnancyViewSet, ListUsersView, UserDetailView, UpdateUserInfoView, ChangePasswordView
+from .views import KakaoLoginCallbackView, RegisterView, LoginView, TokenRefreshView, PregnancyViewSet, ListUsersView, UserDetailView, UpdateUserInfoView, ChangePasswordView
 
 pregnancy_router = DefaultRouter()
 pregnancy_router.register(r'pregnancies', PregnancyViewSet, basename='pregnancy')
@@ -16,4 +16,6 @@ urlpatterns = [
     path('users/<uuid:user_id>/', UserDetailView.as_view(), name='user-detail'),  # 사용자 상세 조회
     path('users/me/', UpdateUserInfoView.as_view(), name='user-me'),  # 현재 사용자 정보 수정
     path('users/me/change-password/', ChangePasswordView.as_view(), name='change-password'), # 현재 사용자 비밀번호 변경
+    
+    path('kakao/callback/', KakaoLoginCallbackView.as_view(), name='kakao-callback'),
 ]
