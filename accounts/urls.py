dafb20bb421baab83bcd45107a9243def8c2a984
ouxrlo <ouxrlo3@gmail.com>
2025-03-07 14:43:27 +0900

    fix: code change

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from streamlit import login
 
 from .views import (RegisterView, LoginView, TokenRefreshView, PregnancyViewSet, ListUsersView,
                     UserDetailView, UpdateUserInfoView, ChangePasswordView, PasswordResetSendCodeView,
@@ -12,7 +11,7 @@ pregnancy_router.register(r'pregnancies', PregnancyViewSet, basename='pregnancy'
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),  # 회원가입
-    path('login/', include(login.urls)),  # 로그인 관련 API
+    path('login/', LoginView.as_view(), name='login'),  # 로그인
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # 토큰 갱신
 
     path('', include(pregnancy_router.urls)),  # 임신 관련 API
@@ -20,5 +19,9 @@ urlpatterns = [
     path('users/', ListUsersView.as_view(), name='user-list'),  # 사용자 목록 조회
     path('users/<uuid:user_id>/', UserDetailView.as_view(), name='user-detail'),  # 사용자 상세 조회
     path('users/me/', UpdateUserInfoView.as_view(), name='user-me'),  # 현재 사용자 정보 수정
+    path('users/me/change-password/', ChangePasswordView.as_view(), name='change-password'), # 현재 사용자 비밀번호 변경
+
+    path('login/send-code', PasswordResetSendCodeView.as_view(), name="send-code"),  # 이메일 인증 - 코드 전송
+    path('login/check-code', PasswordResetCheckView.as_view(), name="check-code"),
 
 ]
