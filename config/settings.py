c003e25379293cb13f3337b3923378cf68397425
ouxrlo <ouxrlo3@gmail.com>
2025-03-11 18:31:01 +0900

    chore: add migration files

--- config/settings.py
+++ config/settings.py
@@ -170,6 +170,7 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASSWORD", ""),
         "HOST": os.getenv("DB_HOST", "localhost"),
         "PORT": os.getenv("DB_PORT", "5432"),
+        'OPTIONS':os.getenv("OPTIONS", ""),
     }
 }
 
