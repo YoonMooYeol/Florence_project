b82810c8bca3a781e27bd827e47063610f0a8a5d
hanmind <a01064864957@gmail.com>
2025-03-13 20:55:05 +0900

    feat: 아기 태교일기 CRUD API 추가

--- calendars/urls.py
+++ calendars/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EventViewSet, DailyConversationSummaryViewSet
+from .views import EventViewSet, DailyConversationSummaryViewSet, BabyDiaryViewSet
 
 router = DefaultRouter()
 router.register('events', EventViewSet, basename='event')
 router.register('conversation-summaries', DailyConversationSummaryViewSet, basename='conversation-summary')
+router.register('baby-diaries', BabyDiaryViewSet, basename='baby-diary')
 
 urlpatterns = [
     path('', include(router.urls)),
