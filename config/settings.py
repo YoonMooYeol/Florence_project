2c10af0d11dcdbe70df808a0180194aa39b2b835
YoonMooYeol <agecross@naver.com>
2025-03-25 10:13:32 +0900

    ALLOWED_HOSTS 및 CORS 설정 업데이트: 모든 호스트를 'nooridal.com' 및 'www.nooridal.com'으로 제한하고, 보안 관련 설정 추가

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ django_env = os.environ.get('DJANGO_ENV')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']  # 모든 호스트 허용
+ALLOWED_HOSTS = ['nooridal.com', 'www.nooridal.com']
+# 만약 www.nooridal.com도 사용한다면:
+# ALLOWED_HOSTS = ['nooridal.com', 'www.nooridal.com']
 
 
 # Application definition
@@ -92,18 +94,24 @@ MIDDLEWARE = [
 ROOT_URLCONF = "config.urls"
 
 
-CORS_ALLOW_ALL_ORIGINS = True #TODO: 모든 도메인에서 접근 가능하도록 하는 코드. 배포 후 주석처리
+# CORS_ALLOW_ALL_ORIGINS = True #TODO: 모든 도메인에서 접근 가능하도록 하는 코드. 배포 후 주석처리
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+
+# HSTS 설정 (1년 동안 HTTPS만 허용, 서브도메인 포함, 프리로드 신청)
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = list(default_headers) + [
     "content-type",
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "https://florence-project-fe.vercel.app",
+    "https://nooridal.com",
+    "https://www.nooridal.com",
 ]
 
 
